553d1618ecf05aed17d04c7e31126e3dc1a7d937
James Anderton <james@janderton.com>
2017-09-14 11:06:42 -0500

    updating sample.py to have new format

--- sample.py
+++ sample.py
@@ -1,11 +1,54 @@
 '''
-    Program: NAME OF PROGAM
+    Program: NAME OF PROGRAM
     File: sample.py
-    Autho: James Anderton
+    Author: James Anderton
     Date: 24-Aug-2017
     Purpose: SOME REASON TO EXIST
 '''
 
+#########################################################
+#
+#   SUB: showMenu
+#
+#########################################################
+def showMenu():
+    menuChoice = -99  # Used to get loop started
+
+    while True:
+        try:
+            while menuChoice < 0 or menuChoice > 3:
+                # Show the Menu
+                print("Sample Menu")
+                print("==================================")
+                print("\t1. Sample 1")
+                print("\t2. Sample 2")
+                print("\t3. Sample 3")
+                print("\t0. Exit")
+                print()
+
+                # get the next choice
+                menuChoice = int(input("Select option: "))
+
+            # EndWhile
+            break
+
+        except ValueError:
+            print("Please type in a valid number. \n")
+        # EndTry
+    # EndWhile
+# EndSub
+
+#########################################################
+#
+#                       MAIN
+#
+#########################################################
+
+showMenu()
+
+
+# SAMPLES
+#*****************************************************************************************************
 
 variableNAME = "stuff"
 CONSTANT = 'stuff'
